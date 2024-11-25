a18a86ddfe1c9804858721baae055db978520871
Cilas Handloser <cilashandloser89@gmail.com>
2024-11-25 23:02:42 +0100

    chat mark for your message

--- server/app/actors/ChatMessages.scala
+++ server/app/actors/ChatMessages.scala
@@ -1,6 +1,7 @@
 package actors
 
 import org.apache.pekko.actor.ActorRef
+import play.api.libs.json._
 
 object ChatMessages {
   case class BroadcastMessage(content: String)
@@ -8,4 +9,10 @@ object ChatMessages {
   case class Join(ref: ActorRef)
 
   case class Leave(ref: ActorRef)
+  
+
+  case class ChatMessage(senderId: String, message: String)
+
+  // JSON-Format für Play JSON
+  implicit val chatMessageFormat: Format[ChatMessage] = Json.format[ChatMessage]
 }
--- server/app/actors/ChatSessionActor.scala
+++ server/app/actors/ChatSessionActor.scala
@@ -3,16 +3,24 @@ package actors
 import org.apache.pekko.actor.{Actor, ActorRef, Props}
 import actors.ChatMessages._
 import actors.ChatSessionActor._
+import play.api.libs.json._
+
+
 
 object ChatSessionActor {
-  case class ClientMessage(msg: String)
+  case class ClientMessage(message: String)
+  case class InitMessage(userId: String)
+
+  // JSON-Formate für Play JSON
+  implicit val clientMessageFormat: Format[ClientMessage] = Json.format[ClientMessage]
+  implicit val initMessageFormat: Format[InitMessage] = Json.format[InitMessage]
 
   def props(out: ActorRef, chatActor: ActorRef): Props =
     Props(new ChatSessionActor(out, chatActor))
 }
 
 class ChatSessionActor(out: ActorRef, chatActor: ActorRef) extends Actor {
-
+  private var userId: Option[String] = None
   override def preStart(): Unit = {
     chatActor ! Join(self)
   }
@@ -20,16 +28,36 @@ class ChatSessionActor(out: ActorRef, chatActor: ActorRef) extends Actor {
   override def postStop(): Unit = {
     chatActor ! Leave(self)
   }
-  
+
   def receive: Receive = {
-    case msg: String =>
-      println(s"Received raw message: $msg")
-      self ! ClientMessage(msg)
-    case ClientMessage(msg: String) =>
-      println(s"Received message from client: $msg")
-      chatActor ! BroadcastMessage(msg) // Nachricht weiterleiten
+    case rawMessage: String =>
+      println(s"Received raw message: $rawMessage")
+      handleMessage(rawMessage)
+
     case BroadcastMessage(content) =>
-      println(s"Received broadcast: $content")
-      out ! content // Nachricht an WebSocket-Client senden
+      println(s"Broadcasting message: $content")
+      out ! content
   }
+
+  private def handleMessage(rawMessage: String): Unit = {
+    val parsedJson = Json.parse(rawMessage)
+    (parsedJson \ "type").asOpt[String] match {
+      case Some("init") =>
+        userId = (parsedJson \ "userId").asOpt[String]
+        println(s"User initialized with ID: ${userId.getOrElse("unknown")}")
+
+      case Some("message") =>
+        val content = (parsedJson \ "content").asOpt[String].getOrElse("")
+        userId.foreach { id =>
+          val message = Json.stringify(Json.toJson(ChatMessages.ChatMessage(id, content)))
+          chatActor ! BroadcastMessage(message)
+        }
+
+      case _ =>
+        println("Unknown message type received")
+    }
+  }
+
+
+
 }
