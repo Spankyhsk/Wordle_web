7215ae4643209b250060fee40324511f4edf1c61
Cilas Handloser <cilashandloser89@gmail.com>
2025-01-07 00:14:21 +0100

    changes with Cookies2

--- server/app/Filters.scala
+++
@@ -1,7 +0,0 @@
-import play.api.http.DefaultHttpFilters
-import play.filters.cors.CORSFilter
-
-import javax.inject.Inject
-
-class Filters @Inject() (corsFilter: CORSFilter)
-  extends DefaultHttpFilters(corsFilter)
